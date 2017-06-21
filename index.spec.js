59c23a68af4c00fdc7aa7d35e055db72fc85f3ae
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-06-21 10:13:59 -0400

    Update names to match issue comment

--- index.spec.js
+++ index.spec.js
@@ -2,22 +2,19 @@ const getFlattenedObject = require('./index');
 
 test('getFlattenedObject should flatten an object', () => {
   const myObject = {
-    arbitraryKey: 'arbitraryValue',
-    nestedObject: {
-      arbitraryKey: 'arbitraryValue'
+    key: 'value',
+    nested: {
+      key: 'value'
     }
   };
 
   const expectedResult = {
-    arbitraryKey: 'arbitraryValue',
-    'nestedObject.arbitraryKey': 'arbitraryValue'
+    key: 'value',
+    'nested.key': 'value'
   };
 
   // Passes as expected
   expect(getFlattenedObject(myObject)).toEqual(expectedResult);
   // Fails because it's using dot notation to search for the property
-  expect(getFlattenedObject(myObject)).toHaveProperty(
-    'nestedObject.arbitraryKey',
-    'arbitraryValue'
-  );
+  expect(getFlattenedObject(myObject)).toHaveProperty('nested.key', 'value');
 });
